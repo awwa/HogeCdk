233d0bec80936800339b1c60aaac030978e812f9
awwa500@gmail.com <awwa500@gmail.com>
2022-10-29 17:36:59 +0900

    Initial commit

---
+++ src/HogeCdk/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace HogeCdk
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new HogeCdkStack(app, "HogeCdkStack");
+
+            app.Synth();
+        }
+    }
+}
